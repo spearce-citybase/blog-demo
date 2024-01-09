0f0b93c3e133ad3adda4cc953076b359dd3bcee9
Steve Pearce <spearce@thecitybase.com>
2024-01-09 12:54:21 -0500

    adding some options to seed service

--- app/services/comparison_service.rb
+++ app/services/comparison_service.rb
@@ -4,53 +4,40 @@ class ComparisonService
   class << self
 
 
-    def compare_each_and_find_each(batch_size: 100)
-        Benchmark.memory do |x|
-          x.report('find_each') do
-            Post.uncached do
-              Post.find_each(batch_size: batch_size) { |p| p.to_json }
-            end
-          end
-          x.report('each') do
-            Post.uncached do
-              Post.all.each { |p| p.to_json }
-            end
-          end
-          x.compare! memory: :allocated
-          x.compare! memory: :retained
-        end
-    end
+    def compare_each_and_find_each(limit: 10_000)
+      Rails.logger.silence do
 
-    def compare_create_and_insert_all
-      Benchmark.memory do |x|
-        x.report('insert_all') do
-          RUN_EXAMPLES_THIS_MANY_TIMES.times do
-            SeedService.seed_profiles
+        Benchmark.memory do |x|
+          x.report('each') do
+            Post.limit(limit).each { |p| p.inspect }
+            GC.start
           end
-        end
-        x.report('create') do
-          RUN_EXAMPLES_THIS_MANY_TIMES.times do
-            SeedService.seed_profiles_slow
+          x.report('find_each') do
+            Post.limit(limit).find_each { |p| p.inspect }
+            GC.start
           end
+          
+          x.compare!
         end
-        x.compare! memory: :allocated
-        x.compare! memory: :retained
       end
     end
 
-    def compare_select_and_pluck
-      Benchmark.memory do |x|
-        x.report('select') do
-          Post.uncached do
-            Post.select(:title).map(&:title).to_json
+    def compare_create_and_insert_all(num: 10_000)
+      Rails.logger.silence do
+        attributes = SeedService.post_attributes(num: num)
+
+        Benchmark.memory do |x|
+          x.report('insert_all') do
+            Post.insert_all attributes
+            GC.start
           end
-        end
-        x.report('pluck') do
-          Post.uncached do
-            Post.pluck(:title).to_json
+          x.report('create') do
+            Post.create attributes
+            GC.start
           end
+
+          x.compare!
         end
-        x.compare!
       end
     end
   end
--- app/services/seed_service.rb
+++ app/services/seed_service.rb
@@ -1,31 +1,22 @@
 class SeedService
-  NUM_PROFILES = 1_000
-  NUM_POSTS = 10_000
-
   class << self
-    def seed_all
+    def seed_all(num_posts: 100_000)
       ActiveRecord::Base.logger.silence do
-        seed_profiles
-        seed_posts
-        seed_comments
-        seed_tags
+        seed_profiles(num: num_posts / 100)
+        seed_posts(num: num_posts)
+        seed_comments(num: num_posts * 3)
       end
       puts "All done!"
     end
 
-    def seed_profiles
+    def seed_profiles(num: 1_000)
       puts "Seeding profiles..."
       Profile.delete_all
-      Profile.insert_all profiles_attributes
+      Profile.insert_all profiles_attributes(num: num)
     end
 
-    def seed_profiles_slow
-      Profile.delete_all
-      Profile.create profiles_attributes
-    end
-
-    def profiles_attributes
-      (1..NUM_PROFILES).each_with_object([]) do |_num, array|
+    def profiles_attributes(num: 1_000)
+      (1..num).each_with_object([]) do |_num, array|
         array << {
           name: FFaker::Name.name,
           created_at: now,
@@ -34,11 +25,17 @@ class SeedService
       end
     end
 
-    def seed_posts
+    def seed_posts(num: 100_000)
       puts "Seeding posts..."
       Post.delete_all
 
-      attributes = (1..NUM_POSTS).each_with_object([]) do |_num, array|
+      post_attributes(num: num).each_slice(10_000) do |attributes|
+        Post.insert_all attributes
+      end
+    end
+
+    def post_attributes(num: 100_000)
+      (1..num).each_with_object([]) do |_num, array|
         array << {
           profile_id: profile_ids.sample,
           title: FFaker::Lorem.sentence,
@@ -47,15 +44,19 @@ class SeedService
           updated_at: now
         }
       end
-
-      Post.insert_all attributes
     end
 
-    def seed_comments
+    def seed_comments(num: 300_000)
       puts "Seeding comments..."
       Comment.delete_all
 
-      attributes = (1..NUM_POSTS * 3).each_with_object([]) do |_num, array|
+      comment_attributes(num: num).each_slice(10_000) do |attributes|
+        Comment.insert_all attributes
+      end
+    end
+
+    def comment_attributes(num: 300_000)
+      (1..num).each_with_object([]) do |_num, array|
         array << {
           profile_id: profile_ids.sample,
           post_id: post_ids.sample,
@@ -65,24 +66,6 @@ class SeedService
           updated_at: now
         }
       end
-
-      Comment.insert_all attributes
-    end
-
-    def seed_tags
-      puts "Seeding tags..."
-      Tag.delete_all
-
-      attributes = (1..NUM_POSTS * 3).each_with_object([]) do |_num, array|
-        array << {
-          post_id: post_ids.sample,
-          value: FFaker::Lorem.word,
-          created_at: now,
-          updated_at: now
-        }
-      end
-
-      Tag.insert_all attributes
     end
 
     def profile_ids
